ae4ff85e0861b1743e80f7c172a0f854e4eac990
CRISHTIANCRH <113752466+CRISHTIANCRH@users.noreply.github.com>
2022-09-17 17:29:12 -0500

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,57 @@
+
+// typing text animation script
+var typed = new Typed(".typing", {
+    strings: ["LA RATATOUILLE"],
+    typeSpeed: 20,
+    backSpeed:40,
+    startdelay:200,
+    loop:true,
+    cursorChar:'|',
+    showCursor: true,
+    loopCount: false,
+});  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+window.sr = ScrollReveal();
+    sr.reveal('.nav', {
+    duration: 3000,
+});
+window.sr = ScrollReveal();
+    sr.reveal('.sub-header', {
+    duration: 3000,
+});
+window.sr = ScrollReveal();
+    sr.reveal('.header-img', {
+    duration: 3000,
+});
+/*window.sr = ScrollReveal();
+    sr.reveal('.header-img', {
+    duration: 2000,
+    origin: 'left',
+    distance: '100px'
+});*/
